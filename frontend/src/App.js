0b984f1f96c7fc67ed9bc8a74b3c9dd51e408747
kpatenio <patenio@ualberta.ca>
2021-02-23 18:42:10 -0800

    added frontend test code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,60 @@
+import {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Table from 'react-bootstrap/Table';
+
+const axios = require('axios').default;
 
 function App() {
+
+  const [testData, setTestData] = useState([]);
+  const [alerts, setAlerts] = useState([])
+
+  // const test = () => {
+  //   axios.get('http://127.0.0.1:8000/')
+  //     .then((response) => {
+  //       setTestData(response.data);
+  //     })
+  // }
+
   return (
     <div className="App">
+      {}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Test Django + React App</h1>
       </header>
+
+      <main>
+      <Container>
+          <div className="buttons">
+            <Button variant="primary" onClick={() => console.log("test")}>Alert</Button>{' '}
+            <Button variant="secondary">Clear</Button>{' '}
+          </div>
+
+          <h2>Users</h2>
+          <Table striped bordered hover variant="dark">
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Full Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {testData.map(e => (
+                <tr>
+                  <td>{e.id}</td>
+                  <td>{e.name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+      </Container>
+      </main>
     </div>
   );
 }
